a79da238321ad5281c813c15bbd44ab7132099f3
Ramsundar Anand <ramsundar.anand@gmail.com>
2024-02-03 16:52:59 +0530

    update to q1.py

--- q1.py
+++ q1.py
@@ -1,21 +1,10 @@
 # Importing libraries
-import time, psutil, os, math
-from tqdm.contrib import itertools
+import math
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns
-sns.set_theme()
 from sklearn.model_selection import train_test_split
 
-# Runtime and memory usage
-start = time.time()
-process = psutil.Process(os.getpid())
-
-# Loading the data
-data = pd.read_csv('training.csv')
-print(pd.Series({"Dataset shape": "{}".format(data.shape)}).to_string())
-data.head()
 
 # Logistic function
 def logistic(x):
@@ -28,6 +17,7 @@ def logistic(x):
     """
     y = 1 / (1 + np.exp(-x))
     return y
+
 
 # Log loss
 def log_loss(y, y_dash):
@@ -42,8 +32,8 @@ def log_loss(y, y_dash):
     loss = - (y * np.log(y_dash)) - ((1 - y) * np.log(1 - y_dash))
     return loss
 
-# Cost function - using vectorizationimport pandas as pd
 
+# Cost function - using vectorization
 def cost_func_vec(y, y_dash):
     """
     Computes log loss for inputs true value (0 or 1) and predicted value (between 0 and 1)
@@ -58,6 +48,7 @@ def cost_func_vec(y, y_dash):
     loss_vec = np.array([log_loss(y[i], y_dash[i]) for i in range(m)])
     cost = np.dot(loss_vec, np.ones(m)) / m
     return cost
+
 
 # Function to compute cost function in terms of model parameters - using vectorization
 def cost_logreg_vec(X, y, w, b):
@@ -79,8 +70,9 @@ def cost_logreg_vec(X, y, w, b):
     cost = cost_func_vec(y, y_dash)
     return cost
 
+
 # Function to compute gradients of the cost function with respect to model parameters - using vectorization
-def grad_logreg_vec(X, y, w, b): 
+def grad_logreg_vec(X, y, w, b):
     """
     Computes gradients of the cost function with respect to model parameters
     Args:
@@ -101,9 +93,8 @@ def grad_logreg_vec(X, y, w, b):
     return grad_w, grad_b
 
 
-
 # Gradient descent algorithm for logistic regression
-def grad_desc(X, y, w, b, alpha, n_iter, show_cost = True, show_params = False): 
+def grad_desc(X, y, w, b, alpha, n_iter, show_cost=True, show_params=False):
     """
     Implements batch gradient descent algorithm to learn and update model parameters
     with prespecified number of interations and learning rate
@@ -124,11 +115,11 @@ def grad_desc(X, y, w, b, alpha, n_iter, show_cost = True, show_params = False):
     assert len(y) == m, "Number of feature observations and number of target observations do not match"
     assert len(w) == n, "Number of features and number of weight parameters do not match"
     cost_history, params_history = [], []
-    for i, j in itertools.product(range(n_iter), range(1)):
-        grad_w, grad_b = grad_logreg_vec(X, y, w, b)   
+    for i in range(n_iter):
+        grad_w, grad_b = grad_logreg_vec(X, y, w, b)
         w += - alpha * grad_w
         b += - alpha * grad_b
-        cost =  cost_logreg_vec(X, y, w, b)
+        cost = cost_logreg_vec(X, y, w, b)
         cost_history.append(cost)
         params_history.append([w, b])
         if show_cost == True and show_params == False and (i % math.ceil(n_iter / 10) == 0 or i == n_iter - 1):
@@ -137,78 +128,6 @@ def grad_desc(X, y, w, b, alpha, n_iter, show_cost = True, show_params = False):
             print(f"Iteration {i:6}:    Cost  {float(cost_history[i]):3.4f},    Params  {params_history[i]}")
     return w, b, cost_history, params_history
 
-X, y, w, b, alpha, n_iter = np.array([[0.1, 0.2], [-0.1, 0.1]]), np.array([1, 0]), np.array([0., 0.]), 0., 0.1, 100000
-w_out, b_out, cost_history, params_history = grad_desc(X, y, w, b, alpha, n_iter, show_cost = True, show_params = True)
-
-
-
-
-#####################################################################################################
-
-# Dropping unnecessary columns
-data.drop(['EventId', 'Weight'], axis = 1, inplace = True)
-
-# Replacing -999 with nan
-data.replace(to_replace = -999, value = np.nan, inplace = True)
-
-# Encoding the 'Label' column
-label_dict = {'b': 0, 's': 1}
-data.replace({'Label': label_dict}, inplace = True)
-
-# Train-test split
-data_train, data_test = train_test_split(data, test_size = 0.2, random_state = 40)
-# Columns with missing values with respective proportions
-(data.isna().sum()[data.isna().sum() > 0] / len(data)).sort_values(ascending = False)
-
-# Discarding columns with more than 30% missing data
-cols_missing_drop = [
-    'DER_deltaeta_jet_jet',
-    'DER_mass_jet_jet',
-    'DER_prodeta_jet_jet',
-    'DER_lep_eta_centrality',
-    'PRI_jet_subleading_pt',
-    'PRI_jet_subleading_eta',
-    'PRI_jet_subleading_phi',
-    'PRI_jet_leading_pt',
-    'PRI_jet_leading_eta',
-    'PRI_jet_leading_phi'
-]
-data_train.drop(cols_missing_drop, axis = 1, inplace = True)
-data_test.drop(cols_missing_drop, axis = 1, inplace = True)
-
-# Median imputation
-data_train['DER_mass_MMC'].fillna(data_train['DER_mass_MMC'].median(), inplace = True)
-data_test['DER_mass_MMC'].fillna(data_test['DER_mass_MMC'].median(), inplace = True)
-
-# Features-target split
-X_train, y_train = data_train.drop('Label', axis = 1), data_train['Label']
-X_test, y_test = data_test.drop('Label', axis = 1), data_test['Label']
-
-# Min-Max normalization
-for col in X_train.columns:
-    if (X_train[col].dtypes == 'int64' or X_train[col].dtypes == 'float64') and X_train[col].nunique() > 1:
-        X_train[col] = (X_train[col] - X_train[col].min()) / (X_train[col].max() - X_train[col].min())
-for col in X_test.columns:
-    if (X_test[col].dtypes == 'int64' or X_test[col].dtypes == 'float64') and X_test[col].nunique() > 1:
-        X_test[col] = (X_test[col] - X_test[col].min()) / (X_test[col].max() - X_test[col].min())
-
-
-
-#####################################################################################################
-
-
-# Initial values of the model parameters
-w_init = np.array([-5, -15, -10, 9, 4, -6, 3, -10, 1, 14, 0, 0, 15, 0, 0, 7, 0, -3, 1, -8]).astype(float)
-b_init = -1.
-
-# Learning model parameters using gradient descent algorithm
-#Change n_iter as per convenience
-w_out, b_out, cost_history, params_history = grad_desc(X_train.to_numpy(),
-                                                       y_train.to_numpy(),
-                                                       w = w_init, # np.zeros(X_train.shape[1]),
-                                                       b = b_init, # 0,
-                                                       alpha = 0.1,
-                                                       n_iter = 200)
 
 # Function to compute confusion matrix
 def conf_mat(y_test, y_pred):
@@ -221,11 +140,12 @@ def conf_mat(y_test, y_pred):
       confusion_mat (array): A 2D array representing a 2x2 confusion matrix
     """
     y_test, y_pred = list(y_test), list(y_pred)
-    count, labels, confusion_mat = len(y_test), [0, 1], np.zeros(shape = (2, 2), dtype = int)
+    count, labels, confusion_mat = len(y_test), [0, 1], np.zeros(shape=(2, 2), dtype=int)
     for i in range(2):
         for j in range(2):
             confusion_mat[i][j] = len([k for k in range(count) if y_test[k] == labels[i] and y_pred[k] == labels[j]])
     return confusion_mat
+
 
 # Function to compute accuracy
 def accuracy(y_test, y_pred):
@@ -238,14 +158,75 @@ def accuracy(y_test, y_pred):
       acc (float): accuracy obtained from y_test and y_pred
     """
     confusion_mat = conf_mat(y_test, y_pred)
-    num = confusion_mat[0, 0] + confusion_mat[1, 1] # Number of correct predictions
-    denom = num + confusion_mat[0, 1] + confusion_mat[1, 0] # Number of total predictions
+    num = confusion_mat[0, 0] + confusion_mat[1, 1]  # Number of correct predictions
+    denom = num + confusion_mat[0, 1] + confusion_mat[1, 0]  # Number of total predictions
     acc = num / denom
     return acc
 
-# Prediction and evaluation on the training set and the test set
-y_train_prob = logistic(np.matmul(X_train.to_numpy(), w_out) + (b_out * np.ones(X_train.shape[0])))
-y_test_prob = logistic(np.matmul(X_test.to_numpy(), w_out) + (b_out * np.ones(X_test.shape[0])))
-y_train_pred, y_test_pred = (y_train_prob > 0.5).astype(int), (y_test_prob > 0.5).astype(int)
-print(pd.Series({"Training accuracy": accuracy(y_train, y_train_pred),
-                 "Test accuracy": accuracy(y_test, y_test_pred)}).to_string())
+
+if __name__ == "__main__":
+    # Loading the data
+    data = pd.read_csv('training.csv')
+    print(pd.Series({"Dataset shape": "{}".format(data.shape)}).to_string())
+    data.head()
+
+    # Dropping unnecessary columns
+    data.drop(['EventId', 'Weight'], axis=1, inplace=True)
+    # Replacing -999 with nan
+    data.replace(to_replace=-999, value=np.nan, inplace=True)
+    # Encoding the 'Label' column
+    label_dict = {'b': 0, 's': 1}
+    data.replace({'Label': label_dict}, inplace=True)
+    # Train-test split
+    data_train, data_test = train_test_split(data, test_size=0.2, random_state=40)
+    # Columns with missing values with respective proportions
+    (data.isna().sum()[data.isna().sum() > 0] / len(data)).sort_values(ascending=False)
+    # Discarding columns with more than 30% missing data
+    cols_missing_drop = [
+        'DER_deltaeta_jet_jet',
+        'DER_mass_jet_jet',
+        'DER_prodeta_jet_jet',
+        'DER_lep_eta_centrality',
+        'PRI_jet_subleading_pt',
+        'PRI_jet_subleading_eta',
+        'PRI_jet_subleading_phi',
+        'PRI_jet_leading_pt',
+        'PRI_jet_leading_eta',
+        'PRI_jet_leading_phi'
+    ]
+    data_train.drop(cols_missing_drop, axis=1, inplace=True)
+    data_test.drop(cols_missing_drop, axis=1, inplace=True)
+    # Median imputation
+    data_train['DER_mass_MMC'].fillna(data_train['DER_mass_MMC'].median(), inplace=True)
+    data_test['DER_mass_MMC'].fillna(data_test['DER_mass_MMC'].median(), inplace=True)
+    # Features-target split
+    X_train, y_train = data_train.drop('Label', axis=1), data_train['Label']
+    X_test, y_test = data_test.drop('Label', axis=1), data_test['Label']
+    # Min-Max normalization
+    for col in X_train.columns:
+        if (X_train[col].dtypes == 'int64' or X_train[col].dtypes == 'float64') and X_train[col].nunique() > 1:
+            X_train[col] = (X_train[col] - X_train[col].min()) / (X_train[col].max() - X_train[col].min())
+    for col in X_test.columns:
+        if (X_test[col].dtypes == 'int64' or X_test[col].dtypes == 'float64') and X_test[col].nunique() > 1:
+            X_test[col] = (X_test[col] - X_test[col].min()) / (X_test[col].max() - X_test[col].min())
+
+    # Initial values of the model parameters
+    w_init = np.array([-5, -15, -10, 9, 4, -6, 3, -10, 1, 14, 0, 0, 15, 0, 0, 7, 0, -3, 1, -8]).astype(float)
+    b_init = -1.
+
+    # Learning model parameters using gradient descent algorithm
+    # Change n_iter as per convenience
+    w_out, b_out, cost_history, params_history = grad_desc(X_train.to_numpy(),
+                                                           y_train.to_numpy(),
+                                                           w=w_init,  # np.zeros(X_train.shape[1]),
+                                                           b=b_init,  # 0,
+                                                           alpha=0.1,
+                                                           n_iter=200)
+
+    # Prediction and evaluation on the training set and the test set
+    y_train_prob = logistic(np.matmul(X_train.to_numpy(), w_out) + (b_out * np.ones(X_train.shape[0])))
+    y_test_prob = logistic(np.matmul(X_test.to_numpy(), w_out) + (b_out * np.ones(X_test.shape[0])))
+    y_train_pred, y_test_pred = (y_train_prob > 0.5).astype(int), (y_test_prob > 0.5).astype(int)
+    print(pd.Series({"Training accuracy": accuracy(y_train, y_train_pred),
+                     "Test accuracy": accuracy(y_test, y_test_pred)}).to_string())
+    pass
